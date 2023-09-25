774c07aa557b5fa87cb13061719133f52c79a2c9
İrem Nur İzci <irererem545@icloud.com>
2023-09-25 16:12:10 +0300

    first commit

---
+++ toplam.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+main() 
+{
+	int i,toplam=0;
+	float ortalama=0;
+	for (i=1;i<100;i++);
+	toplam = toplam+i;
+	ortalama = toplam/100;
+	{
+	printf("toplam: %d\n", toplam);
+	printf("ortalama: %f", ortalama);
+    }
+	return 0;
+}
